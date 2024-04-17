577c9e5542e036425e17a0356f5b182b5b515041
Marten <131295106+Marten235@users.noreply.github.com>
2024-04-17 20:02:58 +0200

    Update NPP.py

--- app/model/schedulingAlgorithms/NPP.py
+++ app/model/schedulingAlgorithms/NPP.py
@@ -6,4 +6,44 @@ import time
 # non preemptive priority
 class NPP(SchedulingStrategy):
     def run(self, scheduler):
-        pass
+        processes = sorted(scheduler.get_processes(), key=lambda x: x.priority)
+
+        # check if no process is running
+        if not any(process.getStatus() == Status.RUNNING for process in processes):
+            # choose the process with the highest priority and set it to running
+            for process in processes:
+                if (
+                    process.getStatus() == Status.READY
+                    and process.getArrivalTime() <= scheduler.get_elapsedTime()
+                ):
+                    process.setStatus(Status.RUNNING)
+                    break
+
+        # execute the running process
+        for process in processes:
+            if (
+                process.getStatus() == Status.RUNNING
+                and process.getArrivalTime() <= scheduler.get_elapsedTime()
+            ):
+                if process.getBurstTime() >= scheduler.get_quantumTime():
+                    time.sleep(scheduler.get_quantumTime())
+                    scheduler.set_elapsedTime(
+                        scheduler.get_elapsedTime() + scheduler.get_quantumTime()
+                    )
+                    process.setBurstTime(
+                        process.getBurstTime() - scheduler.get_quantumTime()
+                    )
+
+                else:
+                    time.sleep(process.getBurstTime())
+                    scheduler.set_elapsedTime(
+                        scheduler.get_elapsedTime() + process.getBurstTime()
+                    )
+                    process.setBurstTime(0)
+
+                if process.getBurstTime() == 0:
+                    process.setStatus(Status.COMPLETED)
+                    process.setCompletionTime(scheduler.get_elapsedTime())
+
+                break
+
