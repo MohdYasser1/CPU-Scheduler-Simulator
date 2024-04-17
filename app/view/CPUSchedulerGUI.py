22439867f4a627ffb4b4d3f31aef004328feeed7
Abdulrahman295 <movieshome160@gmail.com>
2024-04-17 17:32:25 +0200

    add: running the schedular using threads example

--- app/view/CPUSchedulerGUI.py
+++ app/view/CPUSchedulerGUI.py
@@ -1,4 +1,3 @@
-
 import customtkinter as ctk
 from CTkMessagebox import CTkMessagebox
 import threading
@@ -10,6 +9,7 @@ from controller.Scheduler import *
 
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("blue")
+
 
 class MainWindow(ctk.CTk):
     def __init__(self):
@@ -24,73 +24,92 @@ class MainWindow(ctk.CTk):
         self.notLiveGUI = NotLiveFrame(self, scheduler)
         self.notLiveGUI.pack(fill=ctk.BOTH, expand=True)
 
+
 class MainFrame(ctk.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
 
         self.master = master
-        self.grid_rowconfigure((0,3), weight=1)
-        self.grid_columnconfigure((0,2), weight=1)
+        self.grid_rowconfigure((0, 3), weight=1)
+        self.grid_columnconfigure((0, 2), weight=1)
 
         self.schedulerType = None
 
-        self.titleLabel = ctk.CTkLabel(self, text="CPU Scheduler", font=ctk.CTkFont(size=70, weight="bold"))
-        self.titleLabel.grid(row = 0, column = 0,
-                             pady=50,
-                             columnspan=3)
+        self.titleLabel = ctk.CTkLabel(
+            self, text="CPU Scheduler", font=ctk.CTkFont(size=70, weight="bold")
+        )
+        self.titleLabel.grid(row=0, column=0, pady=50, columnspan=3)
 
         # Scheduler Selector
-        self.schedulerSelectLayout = ctk.CTkLabel(self, text="Select Scheduler", font=ctk.CTkFont(size=25))
-        self.schedulerSelectLayout.grid(row = 1, column = 0,
-                                         padx=75,
-                                         sticky="w")
-        schedules = ["FCFS", "SJF (Preemptive)", "SJF (Non-Preemptive)", "Priority (Preemptive)", "Priority (Non-Preemptive)", "Round Robin"]
-        self.dropdown = ctk.CTkOptionMenu(self,
-                                           values = schedules,
-                                           command = self.chooseScheduler)
+        self.schedulerSelectLayout = ctk.CTkLabel(
+            self, text="Select Scheduler", font=ctk.CTkFont(size=25)
+        )
+        self.schedulerSelectLayout.grid(row=1, column=0, padx=75, sticky="w")
+        schedules = [
+            "FCFS",
+            "SJF (Preemptive)",
+            "SJF (Non-Preemptive)",
+            "Priority (Preemptive)",
+            "Priority (Non-Preemptive)",
+            "Round Robin",
+        ]
+        self.dropdown = ctk.CTkOptionMenu(
+            self, values=schedules, command=self.chooseScheduler
+        )
         self.dropdown.set("Choose Scheduler")
-        self.dropdown.grid(row = 1, column = 1,
-                           columnspan=2)
-        
+        self.dropdown.grid(row=1, column=1, columnspan=2)
+
         # Ready Process Input
-        self.noOfProcessLayout = ctk.CTkLabel(self, text="Number of Processes ready:", font=ctk.CTkFont(size=25), anchor="e")
-        self.noOfProcessLayout.grid(row = 2, column = 0, padx=75, pady=20, sticky='w')
-        self.noOfProcessEntry = ctk.CTkEntry(self, placeholder_text="Enter no. of processes")
-        self.noOfProcessEntry.grid(row = 2, column = 1,
-                                   columnspan=2)
+        self.noOfProcessLayout = ctk.CTkLabel(
+            self,
+            text="Number of Processes ready:",
+            font=ctk.CTkFont(size=25),
+            anchor="e",
+        )
+        self.noOfProcessLayout.grid(row=2, column=0, padx=75, pady=20, sticky="w")
+        self.noOfProcessEntry = ctk.CTkEntry(
+            self, placeholder_text="Enter no. of processes"
+        )
+        self.noOfProcessEntry.grid(row=2, column=1, columnspan=2)
 
         # Mode Selection
-        self.modeLayout = ctk.CTkLabel(self, text="Select Mode",
-                                        font=ctk.CTkFont(size=35))
-        self.modeLayout.grid(row = 3, column = 0, padx=75, pady=(0,50), sticky="s")
-        self.liveButton = ctk.CTkButton(self, text="Live",
-                                         command=self.liveMode,
-                                         font=ctk.CTkFont(size=35))
+        self.modeLayout = ctk.CTkLabel(
+            self, text="Select Mode", font=ctk.CTkFont(size=35)
+        )
+        self.modeLayout.grid(row=3, column=0, padx=75, pady=(0, 50), sticky="s")
+        self.liveButton = ctk.CTkButton(
+            self, text="Live", command=self.liveMode, font=ctk.CTkFont(size=35)
+        )
         self.processInput = None
-        self.liveButton.grid(row = 3, column = 1,
-                              pady=(0,50), 
-                              sticky="s")
-        self.notLiveButton = ctk.CTkButton(self, text="Not Live",
-                                            command=self.notLiveMode,
-                                            font=ctk.CTkFont(size=35))
-        self.notLiveButton.grid(row = 3, column = 2,
-                                pady=(0,50), 
-                                sticky="s")
-       
-    def chooseScheduler(self, choise):            
+        self.liveButton.grid(row=3, column=1, pady=(0, 50), sticky="s")
+        self.notLiveButton = ctk.CTkButton(
+            self, text="Not Live", command=self.notLiveMode, font=ctk.CTkFont(size=35)
+        )
+        self.notLiveButton.grid(row=3, column=2, pady=(0, 50), sticky="s")
+
+    def chooseScheduler(self, choise):
         self.schedulerType = choise
-    
+
     def inputProcesses(self):
         if self.processInput is None or not self.processInput.winfo_exists():
-            if self.schedulerType == "Priority (Preemptive)" or self.schedulerType == "Priority (Non-Preemptive)":
-                self.processInput = ProcessPriorityInputFrame(self, int(self.noOfProcessEntry.get()))
+            if (
+                self.schedulerType == "Priority (Preemptive)"
+                or self.schedulerType == "Priority (Non-Preemptive)"
+            ):
+                self.processInput = ProcessPriorityInputFrame(
+                    self, int(self.noOfProcessEntry.get())
+                )
             elif self.schedulerType == "Round Robin":
-                self.processInput = ProcessRRInputFrame(self, int(self.noOfProcessEntry.get()))
+                self.processInput = ProcessRRInputFrame(
+                    self, int(self.noOfProcessEntry.get())
+                )
             else:
-                self.processInput = ProcessInputFrame(self, int(self.noOfProcessEntry.get()))
-            self.processInput.attributes("-topmost", 'true')
+                self.processInput = ProcessInputFrame(
+                    self, int(self.noOfProcessEntry.get())
+                )
+            self.processInput.attributes("-topmost", "true")
         else:
-            self.processInput.focus()  
+            self.processInput.focus()
 
     def liveMode(self):
         self.mode = "live"
@@ -104,19 +123,27 @@ class MainFrame(ctk.CTkFrame):
         self.inputProcesses()
 
     def schedule(self):
-        if self.schedulerType == "Priority (Preemptive)" or self.schedulerType == "Priority (Non-Preemptive)":
+        if (
+            self.schedulerType == "Priority (Preemptive)"
+            or self.schedulerType == "Priority (Non-Preemptive)"
+        ):
             burstTime = [int(i[0].get()) for i in self.processInput.processesObject]
             priority = [int(i[1].get()) for i in self.processInput.processesObject]
-            self.processes = [Process(i, j, k) for i, j, k in zip(range(len(burstTime)), burstTime, priority)]
+            self.processes = [
+                Process(i, j, k)
+                for i, j, k in zip(range(len(burstTime)), burstTime, priority)
+            ]
         else:
             burstTime = [int(i.get()) for i in self.processInput.processesObject]
-            self.processes = [Process(i, j) for i, j in zip(range(len(burstTime)), burstTime)]
+            self.processes = [
+                Process(i, j) for i, j in zip(range(len(burstTime)), burstTime)
+            ]
         if self.schedulerType == "Round Robin":
             self.timeQuantum = int(self.processInput.timeQuantum.get())
             print(self.timeQuantum)
         self.processInput.destroy()
         self.processInput.update()
-        
+
         if self.schedulerType == "FCFS":
             self.SchedulingStrategy = FCFS()
         if self.schedulerType == "SJF (Preemptive)":
@@ -129,15 +156,17 @@ class MainFrame(ctk.CTkFrame):
             self.SchedulingStrategy = NPP()
         if self.schedulerType == "Round Robin":
             self.SchedulingStrategy = RR()
-        
 
         if self.mode == "live":
-            self.scheduler = Scheduler(self.SchedulingStrategy, self.processes, isLive=True)
+            self.scheduler = Scheduler(
+                self.SchedulingStrategy, self.processes, live=True
+            )
         elif self.mode == "not live":
-            self.scheduler = Scheduler(self.SchedulingStrategy, self.processes, isLive=False)
+            self.scheduler = Scheduler(
+                self.SchedulingStrategy, self.processes, live=False
+            )
             self.master.notLiveMode(self.scheduler)
             # self.notLiveGUI.grid(column=0,row=0, sticky='nsew')
-
 
             # while(True):
             #     pass
@@ -150,41 +179,55 @@ class ProcessInputFrame(ctk.CTkToplevel):
         self.title("Process Input")
         self.noOfProcess = noOfProcess
         self.processesObject = []
-        self.grid_rowconfigure((0, self.noOfProcess+2), weight=1)
+        self.grid_rowconfigure((0, self.noOfProcess + 2), weight=1)
         self.grid_columnconfigure((0, 1), weight=1)
-        self.burstTimeLayout = ctk.CTkLabel(self, text="Burst Time", font=ctk.CTkFont(size=25))
-        self.burstTimeLayout.grid(row = 0, column = 1)
+        self.burstTimeLayout = ctk.CTkLabel(
+            self, text="Burst Time", font=ctk.CTkFont(size=25)
+        )
+        self.burstTimeLayout.grid(row=0, column=1)
         self.addProcess()
-        self.scheduleButton = ctk.CTkButton(self, text="Schedule", command=master.schedule)
-        self.scheduleButton.grid(row = self.noOfProcess+3, column = 0, columnspan=2, pady=(0,10), sticky='s')
+        self.scheduleButton = ctk.CTkButton(
+            self, text="Schedule", command=master.schedule
+        )
+        self.scheduleButton.grid(
+            row=self.noOfProcess + 3, column=0, columnspan=2, pady=(0, 10), sticky="s"
+        )
 
     def addProcess(self):
-        for i in range(1, self.noOfProcess+1):
-            processId = ctk.CTkLabel(self, text='P'+str(i-1) + ':', font=ctk.CTkFont(size=20))
-            processId.grid(row = i, column = 0, padx=(0,10), sticky='e')
+        for i in range(1, self.noOfProcess + 1):
+            processId = ctk.CTkLabel(
+                self, text="P" + str(i - 1) + ":", font=ctk.CTkFont(size=20)
+            )
+            processId.grid(row=i, column=0, padx=(0, 10), sticky="e")
             burstTime = ctk.CTkEntry(self, placeholder_text="Enter Burst Time")
-            burstTime.grid(row = i, column = 1, pady=(0,2))
+            burstTime.grid(row=i, column=1, pady=(0, 2))
             self.processesObject.append(burstTime)
 
 
 class ProcessPriorityInputFrame(ProcessInputFrame):
     def __init__(self, master, noOfProcess):
         super().__init__(master, noOfProcess)
-        self.priorityLayout = ctk.CTkLabel(self, text="Priority", font=ctk.CTkFont(size=25))
-        self.priorityLayout.grid(row = 0, column = 2)
+        self.priorityLayout = ctk.CTkLabel(
+            self, text="Priority", font=ctk.CTkFont(size=25)
+        )
+        self.priorityLayout.grid(row=0, column=2)
         self.scheduleButton.grid(columnspan=3)
+
     def addProcess(self):
-            for i in range(1, self.noOfProcess+1):
-                processId = ctk.CTkLabel(self, text='P'+str(i-1) + ':', font=ctk.CTkFont(size=20))
-                processId.grid(row = i, column = 0, padx=(0,10), sticky='e')
-                burstTime = ctk.CTkEntry(self, placeholder_text="Enter Burst Time")
-                burstTime.grid(row = i, column = 1, pady=(0,2))
-                priority = ctk.CTkEntry(self, placeholder_text="Enter Priority")
-                priority.grid(row = i, column = 2, pady=(0,2))
-                self.processesObject.append((burstTime, priority))
+        for i in range(1, self.noOfProcess + 1):
+            processId = ctk.CTkLabel(
+                self, text="P" + str(i - 1) + ":", font=ctk.CTkFont(size=20)
+            )
+            processId.grid(row=i, column=0, padx=(0, 10), sticky="e")
+            burstTime = ctk.CTkEntry(self, placeholder_text="Enter Burst Time")
+            burstTime.grid(row=i, column=1, pady=(0, 2))
+            priority = ctk.CTkEntry(self, placeholder_text="Enter Priority")
+            priority.grid(row=i, column=2, pady=(0, 2))
+            self.processesObject.append((burstTime, priority))
+
 
 class ProcessRRInputFrame(ProcessInputFrame):
     def __init__(self, master, noOfProcess):
         super().__init__(master, noOfProcess)
         self.timeQuantum = ctk.CTkEntry(self, placeholder_text="Enter Time Quantum")
-        self.timeQuantum.grid(row = noOfProcess+2, column = 0, columnspan=2)
+        self.timeQuantum.grid(row=noOfProcess + 2, column=0, columnspan=2)
--- tests/FCFS_test.py
+++ tests/FCFS_test.py
@@ -5,35 +5,36 @@ import os
 from app.controller.Scheduler import *
 from app.model.process.Process import *
 from app.model.schedulingAlgorithms.FCFS import *
+import threading
 
 # FCFS with 5 Processes
-print("///// Test(1) /////")
+# print("///// Test(1) /////")
 
-p1 = Process(1, 5, 1, 0)
-p2 = Process(2, 8, 1, 1)
-p3 = Process(3, 7, 1, 2)
-p4 = Process(4, 3, 1, 3)
-p5 = Process(5, 4, 1, 4)
+# p1 = Process(1, 5, 1, 0)
+# p2 = Process(2, 8, 1, 1)
+# p3 = Process(3, 7, 1, 2)
+# p4 = Process(4, 3, 1, 3)
+# p5 = Process(5, 4, 1, 4)
 
-processes = [p1, p2, p3, p4, p5]
-SchedulingStrategy = FCFS()
-scheduler = Scheduler(SchedulingStrategy, processes)
+# processes = [p1, p2, p3, p4, p5]
+# SchedulingStrategy = FCFS()
+# scheduler = Scheduler(SchedulingStrategy, processes)
 
-while True:
-    print("=====================================")
-    print(f"Elapsed Time: {scheduler.get_elapsedTime()}")
-    currentProcesses = scheduler.get_processes()
-    for process in currentProcesses:
-        print(process)
+# while True:
+#     print("=====================================")
+#     print(f"Elapsed Time: {scheduler.get_elapsedTime()}")
+#     currentProcesses = scheduler.get_processes()
+#     for process in currentProcesses:
+#         print(process)
 
-    if not scheduler.has_processes():
-        break
+#     if not scheduler.has_processes():
+#         break
 
-    scheduler.progress()
+#     scheduler.progress()
 
-print("=====================================")
-print(f"Average turnaround time: {scheduler.getAverageTurnaroundTime()}")
-print(f"Average waiting time: {scheduler.getAverageWaitingTime()}")
+# print("=====================================")
+# print(f"Average turnaround time: {scheduler.getAverageTurnaroundTime()}")
+# print(f"Average waiting time: {scheduler.getAverageWaitingTime()}")
 
 # FCFS with 6 Processes and adding a Process in the middle of execution
 # print("///// Test(2) /////")
@@ -129,3 +130,47 @@ print(f"Average waiting time: {scheduler.getAverageWaitingTime()}")
 # print("=====================================")
 # print(f"Average turnaround time: {scheduler.getAverageTurnaroundTime()}")
 # print(f"Average waiting time: {scheduler.getAverageWaitingTime()}")
+
+
+def run_scheduler(scheduler):
+    while True:
+        print("=====================================")
+        print(f"Elapsed Time: {scheduler.get_elapsedTime()}")
+        currentProcesses = scheduler.get_processes()
+        for process in currentProcesses:
+            print(process)
+
+        if not scheduler.has_processes():
+            break
+
+        scheduler.progress()
+
+    print("=====================================")
+    print(f"Average turnaround time: {scheduler.getAverageTurnaroundTime()}")
+    print(f"Average waiting time: {scheduler.getAverageWaitingTime()}")
+
+
+# FCFS with 6 Processes while running the scheduler in the background using threads
+print("///// Test(4) /////")
+
+p1 = Process(1, 5, 1, 0)
+p2 = Process(2, 8, 1, 1)
+p3 = Process(3, 7, 1, 2)
+p4 = Process(4, 3, 1, 3)
+p5 = Process(5, 4, 1, 4)
+
+processes = [p1, p2, p3, p4, p5]
+SchedulingStrategy = FCFS()
+scheduler = Scheduler(SchedulingStrategy, processes)
+
+# Create and start the thread
+thread = threading.Thread(target=run_scheduler, args=(scheduler,))
+thread.start()
+
+# Main thread continues here
+print("Scheduler is running in the background...")
+print("Main thread continues...")
+
+# Wait for some time and add process to the scheduler while it is running
+time.sleep(7)
+scheduler.add_process(Process(6, 6, 1, 7))
